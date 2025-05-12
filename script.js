4aa35f38e282393500bcdf9760f24ae2e2562e68
morara12 <darumarosann@yahoo.co.jp>
2025-05-12 11:41:24 +0900

    デザイン修正、getProfileInformationからpromiseからasync/awaitに修正

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ document.addEventListener("DOMContentLoaded", getProfileInformation());
 const profile = document.getElementById("profile-btn");
 profile.addEventListener("click", () => getProfileInformation());
 
+const articlelist = document.getElementById("article-lists-btn");
+// articlelist.addEventListener("click", () => getProfileInformation());
+
+
+
 // ＜トークン呼び出しのための関数＞
 async function  getToken(){
   const response  = await fetch('./config.json');
@@ -21,52 +26,67 @@ async function  getToken(){
 // }
 
 
-// プロフィール情報取得
 async function getProfileInformation(){
-  const qiitaAccessToken = await getToken();
+  const profileArea = document.getElementById("profile");
 
-  const myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${qiitaAccessToken}`);
-  
-  const requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow"
-  };
-  // https://ja.wikibooks.org/wiki/JavaScript/Headers
-  // headers: myHeaders,
-  // Headersオブジェクトは、Fetch APIを使用する際に、HTTPリクエストやレスポンスのヘッダー情報を操作するためのオブジェクト
-  // 指定したキーを追加している
-  // https://kde.hateblo.jp/entry/2018/10/22/010811
-  //redirectリダイレクトが発生した場合どう処理するかについて指定する。
-// follow : リダイレクト先まで追従してリソースの取得を行う ※デフォルト
-  fetch("https://qiita.com/api/v2/authenticated_user", requestOptions)
-    .then((response) => response.json())
-    .then((result) => {
-      console.log(result);
-      displayProfile(result);// データ確認        // 取得したデータを表示用関数へ渡す
-    })
-    .catch((error) => console.error(error));
-  // https://www.youtube.com/watch?v=pzIxzegWVu8&t=1469s
-  // feach関数JSでHTTPリクエストを送って、HTTPレスポンスを受け取る仕組み/非同期処理
-  // HTTPホームページのファイルとかを受け渡しするときに使うお約束事
-  // HTTPリクエストホームページを見るときに、ホームページを見るときに使うソフト（Webブラウザ）から
-  // ホームページのファイルが置いてあるコンピュータ（Webサーバ）に対して出される「このページをちょうだい」なお願いのこと
-  // HTTPレスポンス（リクエストに対しての返答）
-  // 表示()
+  if (profileArea.querySelector('.display-profile')){
+    return;
+  }else{
+    const qiitaAccessToken = await getToken();
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${qiitaAccessToken}`);
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow"
+    };
+    // https://ja.wikibooks.org/wiki/JavaScript/Headers
+    // headers: myHeaders,
+    // Headersオブジェクトは、Fetch APIを使用する際に、HTTPリクエストやレスポンスのヘッダー情報を操作するためのオブジェクト
+    // 指定したキーを追加している
+    // https://kde.hateblo.jp/entry/2018/10/22/010811
+    //redirectリダイレクトが発生した場合どう処理するかについて指定する。
+    // follow : リダイレクト先まで追従してリソースの取得を行う ※デフォルト
+    try {
+    const response = await fetch("https://qiita.com/api/v2/authenticated_user", requestOptions)
+    const data = await response.json();
+
+    console.log(data);
+    displayProfile(data); 
+    }catch(error){
+        console.error(error);
+    // https://www.youtube.com/watch?v=pzIxzegWVu8&t=1469s
+    // feach関数JSでHTTPリクエストを送って、HTTPレスポンスを受け取る仕組み/非同期処理
+    // HTTPホームページのファイルとかを受け渡しするときに使うお約束事
+    // HTTPリクエストホームページを見るときに、ホームページを見るときに使うソフト（Webブラウザ）から
+    // ホームページのファイルが置いてあるコンピュータ（Webサーバ）に対して出される「このページをちょうだい」なお願いのこと
+    // HTTPレスポンス（リクエストに対しての返答）
+    // 表示()
+}}
 }
 
 function displayProfile(result){
+  // 古い情報を残っているため、ボタンをクリックするたび表示されてしまう
   const profileArea = document.getElementById("profile");
   const div = document.createElement("div");
   div.classList.add("display-profile")
+// divのクラスやIDを取得して、残っていたら、情報をスルーする
+// 情報取得できているかIFで作って、あればreturnさせる処理をする
 
-  const usernameP= document.createElement('p');
-  usernameP.textContent = `ユーザー名: ${result.id}`;
 
-  const userDescriptionP = document.createElement('p');
-  userDescriptionP.classList.add("user-description")
-  userDescriptionP.textContent = `${result.description}`;
+  const profileElement= document.createElement('p');
+  profileElement.textContent = "profile";
+  profileElement.classList.add("profile-element")
+
+  const usernameElement= document.createElement('p');
+  // pタグであることは重要ではない
+  usernameElement.textContent = ` ${result.id}`;
+
+  // 「pタグ」のクラスではなく要素を作っている
+  const userDescriptionElement = document.createElement('p');
+  userDescriptionElement.classList.add("user-description")
+  userDescriptionElement.textContent = `${result.description}`;
 
   // https://magazine.techacademy.jp/magazine/20738
   const img = document.createElement('img');
@@ -75,9 +95,10 @@ function displayProfile(result){
   img.width = "100";
   img.height = "100";
 
+  div.appendChild(profileElement);
   div.appendChild(img);
-  div.appendChild(usernameP);
-  div.appendChild(userDescriptionP);
+  div.appendChild(usernameElement);
+  div.appendChild(userDescriptionElement);
 
   profileArea.appendChild(div);
 }
